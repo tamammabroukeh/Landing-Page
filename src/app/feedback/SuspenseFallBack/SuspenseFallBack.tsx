08a16f4fc562d98a38a2a3a33c229071e2818724
tamammabroukeh <tamammb367@@gmail.com>
2024-05-25 08:09:32 -0700

    fix app router

--- src/app/feedback/SuspenseFallBack/SuspenseFallBack.tsx
+++ src/app/feedback/SuspenseFallBack/SuspenseFallBack.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Suspense } from "react";
 import Loading from "../../../assets/Loading.svg";
-const SuspenseFallback = ({ children }: { children: React.ReactNode }) => {
+const SuspenseFallback = ({ children }: { children?: React.ReactNode }) => {
   return (
     <Suspense
       fallback={
