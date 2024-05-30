ea0e794fc607698c6c9d398d204e59308b515908
tamammabroukeh <tamammb367@@gmail.com>
2024-05-30 05:02:56 -0700

    fix footer

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -1,40 +1,6 @@
 import { FooterContent } from "../../data/data";
 import { CustomLink, Paragraph, HeadingTwo } from "../../components";
-import { motion, useAnimation } from "framer-motion";
-import { useCallback, useEffect, useRef } from "react";
 const Footer = () => {
-  const sectionRef = useRef<HTMLDivElement>(null);
-  const controls = useAnimation();
-  const handleScroll = useCallback(() => {
-    const scrollPosition = window.scrollY + window.innerHeight;
-    const sectionPosition = sectionRef.current?.offsetTop || 0;
-    const sectionHeight = sectionRef.current?.offsetHeight || 0;
-
-    // Calculate the progress of scroll reveal animation
-    let progress = 0;
-    if (scrollPosition > sectionPosition) {
-      progress = (scrollPosition - sectionPosition) / sectionHeight;
-    }
-
-    // Ensure animation stays within bounds
-    const yValue = Math.min(0, -100 + 100 * progress);
-    const opacityValue = Math.min(1, progress);
-
-    // Apply animation based on scroll progress
-    controls.start({
-      x: `${yValue}%`, // Move from -100% to 0% as progress increases
-      opacity: opacityValue, // Fade in as the section scrolls into view
-    });
-  }, [controls]);
-
-  // Attach scroll event listener
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll]);
-
   return (
     <div className="bg-neutral-900">
       <div className="space-y-5 mx-auto md:px-20 my-8">
@@ -51,15 +17,11 @@ const Footer = () => {
         </div>
         <div className="h-[1px] bg-slate-600" />
         <div
-          ref={sectionRef}
           className="flex justify-between flex-wrap mx-2 text-slate-300"
         >
           {FooterContent.map((item) => {
             return (
-              <motion.div
-                initial={{ x: 0, opacity: 0 }}
-                animate={controls}
-                transition={{ duration: 1 }}
+              <div
                 key={item.id}
               >
                 <HeadingTwo classes="" title={item.parent} />
@@ -68,7 +30,7 @@ const Footer = () => {
                     return <li key={index}>{child}</li>;
                   })}
                 </ul>
-              </motion.div>
+              </div>
             );
           })}
         </div>
